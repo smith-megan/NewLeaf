e3aa98409bca6d8a142e2528095b681d98bb6cfa
Megan <smith.megan.design@gmail.com>
2025-03-11 13:20:31 -0700

    image integration test for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { useState } from "react"
 import Nav from "./components/Nav"
+import grain from "./assets/grain.jpg"
 import "./App.css"
 
 function App() {
@@ -8,7 +9,12 @@ function App() {
   return (
     <>
       <Nav />
-      <div className="text-3xl">New Leaf</div>
+      <div className="grid grid-cols-2 bg-black text-white">
+        <div className="text-3xl">New Leaf</div>
+        <div className="w-full h-full">
+          <img src={grain} />
+        </div>
+      </div>
     </>
   )
 }
