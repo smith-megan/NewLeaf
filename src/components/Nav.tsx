7199ad7449e4cfaf029bf81e3e93f5b96c0bf2c2
Megan <smith.megan.design@gmail.com>
2025-03-12 17:09:58 -0700

    additional front page sections and images

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,13 @@
 function Nav() {
   return (
     <>
-      <div className="bg-black text-white">Nav</div>
+      <div className="grid grid-cols-auto grid-flow-col bg-black text-white pt-4">
+        <div className="grid grid-flow-col">
+          <h1>Logo</h1>
+          <h1>New Leaf</h1>
+        </div>
+        <h1>Menu</h1>
+      </div>
     </>
   )
 }
