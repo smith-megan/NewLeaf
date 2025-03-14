e278db672732f732754d510b2cb2161517b37302
Megan <smith.megan.design@gmail.com>
2025-03-14 14:46:33 -0700

    react router implemented and beginning tutorial page work

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-// import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router"
+
 import Nav from "./components/Nav"
 import Home from "./components/Home"
+import Tutorial from "./components/Tutorial"
 import Footer from "./components/Footer"
 import { Analytics } from "@vercel/analytics/react"
 import "./App.css"
 
 function App() {
-  // const [count, setCount] = useState(10)
-
   return (
     <>
       <Analytics />
-      <Nav />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/tutorial" element={<Tutorial />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,8 +1,35 @@
+import grain from "../assets/grain.jpg"
 function Tutorial() {
   return (
     <>
-      <div className="grid grid-cols-auto grid-flow-col bg-black text-white pt-4">
-        <div className="grid grid-flow-col">start</div>
+      <div className="grid grid-cols-auto grid-flow-row bg-black text-white pt-4">
+        <div className="grid grid-cols-auto grid-flow-cols">
+          <div
+            className=" p-6 pt-50 grid text-left gap-4 bg-cover bg-center"
+            style={{
+              backgroundImage: `url(${grain})`,
+              backgroundColor: `rgba(0,0,0,0.2)`,
+              backgroundBlendMode: "multiply",
+            }}
+          >
+            <h1 className="font-forum text-4xl">Welcome to Tutorials</h1>
+            <p className="font-work font-[300]">
+              Informative step by steps for your refinishing journey
+            </p>
+          </div>
+          <button>Beginners path</button>
+        </div>
+      </div>
+      <div className="grid p-3 gap-2 bg-white text-black">
+        <h2 className="text-left">Posts</h2>
+        <div className="grid grid-cols-auto grid-flow-cols">
+          <div className="w-3/5">
+            <img src={grain} className="w-full" />
+            <h2 className="text-left">Title</h2>
+          </div>
+          <p>grid</p>
+          <p>grid</p>
+        </div>
       </div>
     </>
   )
