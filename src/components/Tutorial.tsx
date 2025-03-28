ec9671b8c79fa20f748adb28e250efd222b72a3b
Megan <smith.megan.design@gmail.com>
2025-03-27 23:24:27 -0700

    fixing router with vercel

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -45,7 +45,7 @@ function Tutorial() {
                     Read more {">"}
                   </Link>
                 ) : (
-                  ""
+                  <></>
                 )}
               </p>
             </div>
